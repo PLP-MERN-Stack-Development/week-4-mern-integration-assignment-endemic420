5e9527ec97dc91601ea1d2a3124165aa220221a5
endemic420 <karanjadennis17@gmail.com>
2025-07-08 21:33:21 +0300

    themeToggle complete

---
+++ client/src/components/themeToggle.jsx
@@ -0,0 +1,31 @@
+import { IoMdMoon } from "react-icons/io";
+import { IoMdSunny } from "react-icons/io";
+import {Button} from "@/components/ui/button";
+import { useState, useEffect } from "react";
+
+
+
+
+export default function ThemeToggle (){
+    const [dark, setDark] = useState(
+    () => localStorage.getItem("theme")==="dark"
+    );
+
+    useEffect(()=>{
+        const root = window.document.documentElement;
+
+        if(dark){
+            root.classList.add("dark");
+            localStorage.setItem("theme","dark");
+        }else{
+            root.classList.remove("dark");
+            localStorage.setItem("theme","light");
+        }
+    },[dark]);
+}
+
+return (
+    <Button variant = "ghost" size = "icon" aria-label = "toggleTheme" onClick ={() => setDark (!dark)}>
+        {dark ? <IoMdSunny className ="h-5 w-5"/>:<IoMdMoon className = "h-5 w-5"/>}
+    </Button>
+    );
